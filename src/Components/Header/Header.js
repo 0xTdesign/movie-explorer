ca91589bed8b5c26dddd522b5c6a68093ed8a57d
jimbobdxb <tallisondxb@gmail.com>
2022-10-06 08:44:24 +0400

    updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+// import logo from "../../assets/newLogo.png";
 
 export default function Header({ handleChange, getMovie }) {
   return (
@@ -7,6 +8,7 @@ export default function Header({ handleChange, getMovie }) {
       <div className="header-container">
         <nav className="nav-list">
           <div className="logo">
+            {/* <img className="logo-movie" src={logo} alt="" /> */}
             <h2>Movie Finder</h2>
           </div>
           <ul>
